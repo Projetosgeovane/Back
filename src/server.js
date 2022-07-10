c964d2f369e6c9d03b2c64d814212f9ea480e426
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-07-10 10:58:51 -0300

    a

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const { app } = require('.');
 
 const hostname = "localhost";
 const port = 4000;
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
